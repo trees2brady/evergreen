03f5092cfec597afc6dca29c931714b0cacf7a41
trees <trees2brady@gmail.com>
2022-04-24 21:55:08 -0400

    fix error of retriving pages with 2 digits

--- apps/dishes/urls.py
+++ apps/dishes/urls.py
@@ -4,6 +4,6 @@ from .views import *
 
 urlpatterns = [
     re_path(r'^$', DishView.as_view(), name="dishes"),
-    re_path(r'^(?P<id>\d)$', DishDetail.as_view(), name="dish_detail"),
-    re_path(r'^add_to_cart/(?P<id>\d)$', AddToCart.as_view(), name="add_to_cart"),
+    re_path(r'^(?P<id>[0-9]+)$', DishDetail.as_view(), name="dish_detail"),
+    re_path(r'^add_to_cart/(?P<id>[0-9]+)$', AddToCart.as_view(), name="add_to_cart"),
 ]
